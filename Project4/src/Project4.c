742970790afbf377a9e0bb020dd0e4eb1a17eeac
gulsumgudukbay <gulsum.gudukbay@ug.bilkent.edu.tr>
2016-12-06 19:33:49 +0200

    yo

---
+++ Project4/src/Project4.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Project4.c
+ Author      : Gulsum Gudukbay & Dogukan Yigit Polat
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
