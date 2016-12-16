1e9606b1f87a4b1e286ec358e695a71accd8c779
Gulsum Gudukbay <gudukbay@gmail.com>
2016-12-16 21:37:13 +0300

    BITTI AMK BITTI

--- Project4/src/pagesim.c
+++ Project4/src/pagesim.c
@@ -29,7 +29,7 @@ void enqueue( uint32_t addr)
 {
 	struct node* temp;
 
-	printf( "enqueue 0x%08x\n", addr);
+	//printf( "enqueue 0x%08x\n", addr);
 	if( !queue_head)
 	{
 		queue_head = (struct node*) malloc( sizeof(struct node));
@@ -77,7 +77,6 @@ int requeue( uint32_t addr)
 		if( cur->addr == addr)
 		{
 
-			printf( "r-");
 			if( cur == queue_tail)
 				queue_tail = prev;
 
@@ -120,6 +119,7 @@ struct in_table_entry
 
 struct out_table_entry out_table[1024];
 int M;
+int pagefault_count;
 
 void memory_access( uint32_t addr, int isLRU, FILE* fp)
 {
@@ -133,12 +133,12 @@ void memory_access( uint32_t addr, int isLRU, FILE* fp)
 
 	if( !out_table[outer_table].used)
 	{
-		printf("SEGMANTATION FAULT\n");
+		printf("Invalid read at %x\n", addr);
 		return;
 	}
 	else if( !out_table[outer_table].inner_table[inner_table].used)
 	{
-		printf("SEGMANTATION FAULT\n");
+		printf("Invalid read at %d\n", addr);
 		return;
 	}
 
@@ -149,7 +149,7 @@ void memory_access( uint32_t addr, int isLRU, FILE* fp)
 
 		uint32_t fnum = out_table[outer_table].inner_table[inner_table].frame_number;
 		uint32_t phys_addr = ((fnum << 12) & 0xFFFFF000) | page_offset;
-		printf( "0x%08x\n", phys_addr);
+		//printf( "0x%08x\n", phys_addr);
 	  fprintf(fp,  "0x%08x\n", phys_addr);
 	}
 	else
@@ -163,8 +163,9 @@ void memory_access( uint32_t addr, int isLRU, FILE* fp)
 
 			uint32_t fnum = out_table[outer_table].inner_table[inner_table].frame_number;
 			uint32_t phys_addr = ((fnum << 12) & 0xFFFFF000) | page_offset;
-			printf( "0x%08x x\n", phys_addr);
+			//printf( "0x%08x x\n", phys_addr);
 			fprintf(fp,  "0x%08x x\n", phys_addr);
+			pagefault_count++;
 
 		}
 		else
@@ -174,7 +175,7 @@ void memory_access( uint32_t addr, int isLRU, FILE* fp)
 			if( !err)
 			{
 				uint32_t vic_fnum = out_table[(victim & (0xFFC00000)) >> 22].inner_table[(victim & (0x003FF000)) >> 12].frame_number;
-				printf( "victim is 0x%08x\n", victim);
+				//printf( "victim is 0x%08x\n", victim);
 				out_table[(victim & (0xFFC00000)) >> 22].inner_table[(victim & (0x003FF000)) >> 12].valid = 0;
 				out_table[outer_table].inner_table[inner_table].frame_number = vic_fnum >> 12;
 				out_table[outer_table].inner_table[inner_table].valid = 1;
@@ -182,8 +183,9 @@ void memory_access( uint32_t addr, int isLRU, FILE* fp)
 
 				uint32_t fnum = out_table[outer_table].inner_table[inner_table].frame_number;
 				uint32_t phys_addr = ((fnum << 12) & 0xFFFFF000) | page_offset;
-				printf( "0x%08x x\n", phys_addr);
+				//printf( "0x%08x x\n", phys_addr);
 				fprintf(fp,  "0x%08x x\n", phys_addr);
+				pagefault_count++;
 
 			}
 			else
@@ -238,6 +240,7 @@ void init( struct range_node * ranges)
 	uint32_t Xin, Yin;
 	uint32_t Xout, Yout;
 	uint32_t X, Y;
+	uint32_t Xoff, Yoff;
 	struct range_node *cur;
 
 
@@ -258,6 +261,8 @@ void init( struct range_node * ranges)
 		Xout = (X & (0xFFC00000)) >> 22;
 		Yin = (Y & (0x003FF000)) >> 12;
 		Yout = (Y & (0xFFC00000)) >> 22;
+		Xoff = (X & (0x00000FFF));
+		Yoff = (Y & (0x00000FFF));
 
 		printf("%8x %8x: %x %x %x %x\n", X, Y, Xout, Yout, Xin, Yin);
 
@@ -269,6 +274,7 @@ void init( struct range_node * ranges)
 
 		if(Yout - Xout > 0)
 		{
+
 
 			for(int i = Xin; i < 1024; i++)
 			{
@@ -290,12 +296,19 @@ void init( struct range_node * ranges)
 
 			out_table[Yout].used = 1;
 
-			if(!out_table[Yout].inner_table)
+			if(!out_table[Yout].inner_table){
 				out_table[Yout].inner_table = create_inner_table();
+			}
 
 			for(int i = 0; i < Yin; i++)
 			{
+
 				out_table[Yout].inner_table[i].used = 1;
+			}
+			
+			if( (Yoff-Xoff))
+			{
+				out_table[Yout].inner_table[Yin].used = 1;
 			}
 
 		}
@@ -304,6 +317,11 @@ void init( struct range_node * ranges)
 			for(int i = Xin; i < Yin; i++)
 			{
 				out_table[Xout].inner_table[i].used = 1;
+			}
+
+			if( (Yoff-Xoff))
+			{
+				out_table[Xout].inner_table[Yin].used = 1;
 			}
 		}
 	}
@@ -333,9 +351,8 @@ int arguments( int argc, char** argv, uint32_t* vmsize, int* M,
 
 	if( isRandom)
 	{
-		*input2 = argv[1];
-		*M = atoi(argv[2]);
-		*avutbud = argv[3];
+		*M = atoi(argv[1]);
+		*avutbud = argv[2];
 		*algo = atoi(argv[a_index + 1]);
 		*vmsize = atoi(argv[r_index + 1]);
 	}
@@ -363,32 +380,22 @@ int main(int argc, char** argv)
 	char* in1_name;
 	char* in2_name;
 	int isRandom;
+	uint32_t keko;
 
-	if( argc != 8 && argc != 7)
+	if( argc != 7)
 	{
-		if( argc != 8)
-		{
-			printf("involix inbudu\n");
-			return 1;
-		}
+		printf("involix inbudu\n");
+		return 1;
 	}
 
 	isRandom = arguments( argc, argv, &vmsize, &M, &in1_name,
 												&in2_name, &out_name, &a);
 
 
-
-	in2 = fopen( in2_name, "r+");
 	out	= fopen( out_name, "w+");
 	if( isRandom)
 	{
-		if( argc != 8)
-		{
-			printf("involix inbudu\n");
-			return 1;
-		}
-
-		printf("\nin2: %s \nout: %s \nM: %d \nalgo: %d \nvmsize: %d \n", in2_name, out_name, M, a, vmsize);
+		printf("\nout: %s \nM: %d \nalgo: %d \nvmsize: %d \n", out_name, M, a, vmsize);
 
 		struct range_node myrange;
 		myrange.next = 0;
@@ -399,28 +406,57 @@ int main(int argc, char** argv)
 	}
 	else
 	{
-		if( argc != 7)
-		{
-			printf("involix inbudu\n");
-			return 1;
-		}
-
 		printf("\nin1: %s \nin2: %s \nout: %s \nM: %d \nalgo: %d \n", in1_name, in2_name, out_name, M, a);
 
+		in2 = fopen( in2_name, "r+");
 		in1 = fopen( in1_name, "r+");
 		struct range_node* rng = read_ranges( in1);
+		keko = rng[0].Y;
 		init(rng);
 
 	}
 
-
-	//init(&myrange);
-	uint32_t inputtanokunancisim;
-
-	while(fscanf(in2, "%x", &inputtanokunancisim) != EOF)
+	
+	for( int i = 0; i < 1024; i++)
 	{
-		memory_access(inputtanokunancisim, a, out);
+		if( out_table[i].used)
+		{
+			for( int j = 0; j < 1024; j++)
+			{
+				if( out_table[i].inner_table[j].used)
+				{
+					printf("i j : %d %d\n", i , j );
+				}
+			}
+		}
 	}
+	
+
+	pagefault_count = 0;
+	keko = 0;
+	if( isRandom)
+	{
+		for( int i = 0; i < (vmsize*M) / 10; i++)
+		{
+			uint32_t random_var = rand() % vmsize;
+			//printf("rv= %x\n", random_var);
+			memory_access( random_var, a, out);
+			keko++;
+		}
+	}
+	else
+	{
+		uint32_t inputtanokunancisim;
+		while(fscanf(in2, "%x", &inputtanokunancisim) != EOF)
+		{
+			memory_access(inputtanokunancisim, a, out);
+			keko++;
+		}
+	}
+
+	printf("memory accesses: %d\n", keko);
+
+	printf("page faults: %d\n", pagefault_count);
 
 	fclose(out);
 	return 0;
